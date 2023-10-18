8d4dc30dd18e51eb39c95f8883eea9009f48c880
Michael Stover <michael.r.stover@gmail.com>
2023-10-18 10:04:20 -0400

    test module-info update

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -2,9 +2,16 @@ module coinjema.cjm.test {
     requires coinjema.cjm;
     requires org.apache.groovy;
     requires org.junit.jupiter.api;
+    requires junit;
     requires java.management;
     requires java.logging;
     requires java.sql;
     exports org.coinjema.testing.nontest;
     exports org.coinjema.testing.nontest.noaspect;
+    exports org.coinjema.testing.context;
+    exports org.coinjema.testing.constructor;
+    exports org.coinjema.testing.collections;
+    exports org.coinjema.testing.util;
+    opens org.coinjema.testing.constructor;
+    opens org.coinjema.testing.context;
 }
